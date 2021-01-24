b3c09ae76bc986bc232d24f92fb9e4462e55e15e
Mikko Aro <M9492@student.jamk.fi>
2021-01-24 14:34:18 +0200

    Added 2.9 & 2.11

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,54 +1,90 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const App = () => {
-  const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '044-1231244' }
-  ]) 
-  const [ newName, setNewName ] = useState('')
-  const [ newPhone, setNewPhone ] = useState('')
+  const [ persons, setPersons] = useState([]) 
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
+
+  const [ filteredPersons, setFilteredPersons] = useState([]) 
+  const [ tempPerson, setTempPerson ] = useState({name: '', number: ''});
 
   const addName = (event) => {
     event.preventDefault()
-    const nameObject = {
-      name: newName,
-      number: newPhone
-    }
-    if(persons.some(e => e.name === newName)){
-      window.alert(`${newName} is already added to phonebook`)
+    if(persons.some(e => e.name === tempPerson.name)){
+      window.alert(`${tempPerson.name} is already added to phonebook`)
     } else {
-      setPersons(persons.concat(nameObject))
+      if(tempPerson.name && tempPerson.number) {
+        persons.push(tempPerson)
+      }
     } 
-    setNewName('')
-    setNewPhone('')
+    setTempPerson({name: '', number: ''})
   }
 
   const nameInputChange = (event) => {
-    setNewName(event.target.value)
+    const newPerson = {...tempPerson, ...{ name: event.target.value }}
+    setTempPerson(newPerson)
   }
 
   const phoneInputChange = (event) => {
-    setNewPhone(event.target.value)
+    const newPerson = {...tempPerson, ...{ number: event.target.value }}
+    setTempPerson(newPerson)
+  }
+
+  const filterNameChange = (event) => {
+    setFilteredPersons([]);
+    const filtered = [];
+    persons.forEach(person => {
+      if((person.name.toLowerCase().includes(event.target.value.toLowerCase()))){
+        filtered.push(person);
+      }
+    });    
+    setFilteredPersons(filtered)
   }
 
   return (
     <div>
+      <div>filter shown with: 
+        <input 
+          onChange={filterNameChange} 
+        />
+      </div>
       <h2>Phonebook</h2>
+      <h2>Add new</h2>
       <form onSubmit={addName}>
         <div>Name: 
           <input 
-          value={newName} 
-          onChange={nameInputChange} />
+            value={tempPerson.name} 
+            onChange={nameInputChange} 
+          />
         </div>
-        <div>Number: <input 
-          value={newPhone}
-          onChange={phoneInputChange} />
+        <div>Number: 
+          <input 
+            value={tempPerson.number}
+            onChange={phoneInputChange} 
+          />
         </div>
         <div>
           <button type="submit">Add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
-      {persons.map(person => <ul key={person.name}> {person.name} {person.number} </ul>)}
+      <h2>All persons</h2>
+      {persons.map((person, i) => {
+        return (
+          <ul key={`all-${i}`}> {person.name} {person.number} </ul> 
+        )
+      })}
+      <h2>Filtered persons</h2>
+      {filteredPersons.map((person, i) => {
+        return (
+          <ul key={`filtered-${i}`}> {person.name} {person.number} </ul> 
+        )
+      })}
     </div>
   )
 }
